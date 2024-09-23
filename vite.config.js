2c2c760ec5ff8095dcd32a94faabf08a0602672f
tdp-313 <119167759+tdp-313@users.noreply.github.com>
2024-09-23 23:08:32 +0900

    Fix root folder configuration error

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: 'src',
-    base: '/',
-    publicDir: 'public',
-      build: {
-      outDir: '../dist',
-    }
+  root: 'src',
+  base: './',
+  publicDir: 'public',
+  build: {
+    outDir: '../dist',
+  }
 })
